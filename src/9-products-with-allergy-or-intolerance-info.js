dabddab182df022976f452ced39f45083f665a38
Matheus Pozett <maa_souza11@hotmail.com>
2025-02-28 15:43:53 -0300

    Crie uma função que retorne todos os produtos com informações sobre alergia ou intolerância

--- src/9-products-with-allergy-or-intolerance-info.js
+++ src/9-products-with-allergy-or-intolerance-info.js
@@ -1,7 +1,24 @@
 const stockProducts = require('./data.json');
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsWithAllergyOrIntolerance = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].allergyOrIntolerance) {
+      productsWithAllergyOrIntolerance.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        allergyOrIntoleranceMessage: `Pode conter: ${
+          stockProducts[index].allergyOrIntolerance.join(' ')}`,
+      });
+    }
+  }
+
+  return productsWithAllergyOrIntolerance;
 };
 
+console.log(getProductsWithAllergyOrIntoleranceInfo());
+
 module.exports = { getProductsWithAllergyOrIntoleranceInfo };
+
+// `Pode conter: ${stockProducts[index].allergyOrIntolerance}`
