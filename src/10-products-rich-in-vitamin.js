1047c3e98cfda3fbf3ecb6f0cde36783f7257aa4
Matheus Pozett <maa_souza11@hotmail.com>
2025-02-28 15:56:09 -0300

    Cria uma função que retorne todos os produtos que possuam alguma vitamina em seu valor nutricional

--- src/10-products-rich-in-vitamin.js
+++ src/10-products-rich-in-vitamin.js
@@ -1,7 +1,25 @@
 const stockProducts = require('./data.json');
 
 const getProductsRichInVitamin = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsRichInVitamin = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].nutritionalInfo.vitamins) {
+      const vitaminsObject = stockProducts[index].nutritionalInfo.vitamins;
+      const vitaminsArray = Object.entries(vitaminsObject).map(([key, value]) =>
+        `${key} - ${value}`);
+
+      productsRichInVitamin.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        vitaminsInformation: vitaminsArray,
+      });
+    }
+  }
+
+  return productsRichInVitamin;
 };
+
+console.log(getProductsRichInVitamin());
 
 module.exports = { getProductsRichInVitamin };
