547b289083313d087a5d83d0fc9b4502b4c87aa9
Matheus Pozett <maa_souza11@hotmail.com>
2025-02-28 15:22:09 -0300

    Cria função que busca produtos em promoção

--- src/8-products-on-sale.js
+++ src/8-products-on-sale.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
 const getProductsOnSale = () => {
-  // Desenvolva seu código dentro dessa função...
+  let productsOnSale = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].onSale === true) {
+      productsOnSale.push({
+        description: stockProducts[index].description,
+        formattedPrice: `R$ ${stockProducts[index].price}`,
+        onSale: stockProducts[index].onSale,
+      });
+    }
+  }
+
+  return productsOnSale;
 };
+
+console.log(getProductsOnSale());
 
 module.exports = { getProductsOnSale };
